e2f68a5418eb14ab7a70e8653231c9c408cb2470
trishaldoshi <trishaldoshi@gmail.com>
2022-03-08 14:58:52 +0800

    updated versions

--- provider-declare.tf
+++ provider-declare.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = ">= 2.71"
+      version = ">= 2.98"
     }
   }
-  required_version = ">= 1.0.4"
+  required_version = ">= 1.1.7"
 }
 provider "azurerm" {
   features {}
